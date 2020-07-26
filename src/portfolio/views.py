d5fa97aa82826c02b3de50bae2b2b5b7ec241615
Mohd Omama <mohd.omama@gmail.com>
2020-07-26 11:43:07 +0530

    feat: home page

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def home_page(request):
     context = {'title': 'Home Page'}
-    return render(request, "base.html", context)
+    template_name = 'home.html'
+    return render(request, template_name , context)
