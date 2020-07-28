8537a8e429d6949e22bf3573ad5ad179184a7c5b
Mohd Omama <mohd.omama@gmail.com>
2020-07-28 07:26:33 +0530

    feat: complete blog page

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render
+from .util import rss_parser
 
 
 def home_page(request):
     context = {'title': 'Home'}
     template_name = 'home.html'
-    return render(request, template_name , context)
+    return render(request, template_name, context)
+
 
 def resume_page(request):
     context = {'title': 'Resume'}
     template_name = 'resume.html'
-    return render(request, template_name , context)
+    return render(request, template_name, context)
+
 
 def about_page(request):
     context = {'title': 'About'}
     template_name = 'about.html'
-    return render(request, template_name , context)
+    return render(request, template_name, context)
+
 
 def projects_page(request):
     context = {'title': 'Projects'}
     template_name = 'projects.html'
-    return render(request, template_name , context)
+    return render(request, template_name, context)
+
 
 def blog_page(request):
-    context = {'title': 'Blog'}
+    entries = rss_parser.get_feed()
+    context = {'title': 'Blog', 'entries':entries}
     template_name = 'blog.html'
-    return render(request, template_name , context)
+    return render(request, template_name, context)
